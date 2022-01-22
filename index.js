5a3fcdbcd328a5194cf7389cd912710303cab6cc
unknown <kennyya@naver.com>
2022-01-22 21:00:39 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const mongoose  = require('mongoose')
 const app = express()
 const port = 5000
 
-mongoose.connect('mongodb+srv://kennyya:ky9322ky&@cluster0.h4ayv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+const config = require('./config/key');
+
+mongoose.connect(config.mongoURI,{
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 app.get('/', (req, res) => {
