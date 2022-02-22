b31bb3cd87211e89628b5f12c905398a8162fbf3
unknown <kennyya@naver.com>
2022-02-22 15:13:56 +0900

    first commit

---
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react'
+import { Row, Col, List, Avatar } from 'antd'
+import Axios from 'axios'
+import SideVideo from './Sections/SideVideo'
+import Subscribe from './Sections/Subscribe'
+import Comment from './Sections/Comment'
+
+function VideoDetailPage(props) {
+
+    const videoId = props.match.params.videoId
+    const variable = {videoId: videoId}
+
+    const [VideoDetail, setVideoDetail] = useState([])
+    const [Comments, setComments] = useState([])
+
+
+    useEffect(() => {
+      Axios.post('/api/video/getVideoDetail',variable)
+      .then(response => {
+        if(response.data.success){
+            setVideoDetail(response.data.videoDetail)
+        }else{
+            alert('비디오 가져오기를 실패 했습니다.')
+        }
+      })
+
+      Axios.post('/api/comment/getComments', variable)
+      .then(response=>{
+          if(response.data.success){
+              
+            setComments(response.data.comments)
+            
+            console.log(response.data.comments)
+
+          }else{
+              alert('코멘트 정보를 가져오는 것을 실패 하였습니다.')
+          }
+      })
+    }, [])
+    
+
+    if(VideoDetail.writer){
+
+        const subscribeButton =  VideoDetail.writer._id !==localStorage.getItem('userId') && <Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/>
+        return (
+        <Row gutter={[16, 16]}>
+            <Col lg={18} xs={24}>
+                <div style={{ width: '100%', padding: '3rem 4em' }}>
+                <video style={{ width: '100%' }} src={`http://localhost:5000/${VideoDetail.filePath}`} controls></video>
+               <List.Item
+                actions ={[subscribeButton]}
+                >   
+                    
+                    <List.Item.Meta
+                        avatar={<Avatar src={VideoDetail.writer.image}/>}
+                        title={VideoDetail.writer.name}
+                        description={VideoDetail.description}
+                    />
+                    
+                    
+                </List.Item>
+                <Comment commentLists={Comments} postId={videoId}/>
+               
+               
+                </div>
+    
+            </Col>
+            <Col lg={6} xs={24}>
+                <SideVideo/>
+            </Col>
+        </Row>
+      )
+    
+    }else{
+        return(
+            <div>...</div>
+        )
+    }
+}
+
+export default VideoDetailPage
